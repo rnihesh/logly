e1c55856d1be82d77a1a82a46b95bf1d45f5c8cc
rnihesh <niheshr03@gmail.com>
2025-02-20 00:10:33 +0530

    Revamp3 BackendDeploy

--- server/server.js
+++ server/server.js
@@ -13,11 +13,17 @@ app.use(cors())
 
 const port = process.env.PORT || 4000;
 
+// Basic route to test if server is running
+app.get('/', (req, res) => {
+  res.send('LOGLY API is running');
+});
+
+
 //db connnection
 mongoose
   .connect(process.env.DBURL)
   .then(() => {
-    app.listen(port, () => console.log(`Server listening on port: ${port}`));
+    app.listen(port,'0.0.0.0' , () => console.log(`Server listening on port: ${port}`));
     console.log("DB Connection Success");
   })
   .catch((err) => console.log("Error in DB Connection ", err));
