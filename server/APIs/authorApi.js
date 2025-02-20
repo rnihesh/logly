25955c7746880d8fc2023fcb4c30cae3c1d0e50f
Nihesh Rachakonda <153965219+rnihesh@users.noreply.github.com>
2025-02-20 11:13:35 +0530

    R10

--- server/APIs/authorApi.js
+++ server/APIs/authorApi.js
@@ -7,7 +7,7 @@ const Article = require("../models/articleModel.js");
 const { requireAuth, clerkMiddleware } = require("@clerk/express");
 require("dotenv").config();
 
-// authorApp.use(clerkMiddleware())
+authorApp.use(clerkMiddleware())
 
 //create new author
 authorApp.post("/author", expressAsyncHandler(createUserOrAuthor));
