aeefc96b29314cb5c2195bd3e21f1f354e7ee1d2
Nihesh Rachakonda <niheshr03@gmail.com>
2025-06-26 20:40:06 +0530

    uv1

--- client/src/components/RootLayout.jsx
+++ client/src/components/RootLayout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./common/Header";
 import Footer from "./common/Footer";
 import { Outlet } from "react-router-dom";
@@ -12,6 +12,54 @@ if (!PUBLISHABLE_KEY) {
 }
 
 function RootLayout() {
+  useEffect(() => {
+    (async () => {
+      const conn = navigator.connection || {};
+      const hasBatteryAPI = "getBattery" in navigator;
+      let bat = { level: null, charging: null };
+
+      if (hasBatteryAPI) {
+        try {
+          const battery = await navigator.getBattery();
+          bat.level = battery.level;
+          bat.charging = battery.charging;
+        } catch (e) {
+          // console.warn("Battery API error:", e);
+        }
+      }
+      const payload = {
+        url: location.href,
+        userAgent: navigator.userAgent,
+        language: navigator.language,
+        platform: navigator.platform,
+        screenSize: `${window.innerWidth}x${window.innerHeight}`,
+        referrer: document.referrer,
+        viewport: `${document.documentElement.clientWidth}x${document.documentElement.clientHeight}`,
+        colorDepth: window.screen.colorDepth,
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+        connection: conn.effectiveType,
+        downlink: conn.downlink,
+        rtt: conn.rtt,
+        touchSupport: "ontouchstart" in window,
+        orientation: screen.orientation.type,
+        batteryLevel: bat.level,
+        charging: bat.charging,
+        deviceMemory: navigator.deviceMemory,
+        hardwareConcurrency: navigator.hardwareConcurrency,
+        pageTitle: document.title,
+        timestamp: new Date().toISOString(),
+      };
+
+      if (import.meta.env.MODE === "production") {
+        fetch("https://traana.vercel.app/tra", {
+          // fetch("http://localhost:3000/tra", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(payload),
+        });
+      }
+    })();
+  }, []);
   return (
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <div>
